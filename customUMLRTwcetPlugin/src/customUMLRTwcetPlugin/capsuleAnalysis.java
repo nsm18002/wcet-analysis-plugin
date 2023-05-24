b32cd8c0eca09c943125b68e0b86d023376ccc0f
nsm18002 <58140569+nsm18002@users.noreply.github.com>
2023-05-24 12:32:19 +0200

    Update capsuleAnalysis.java
    Fixed the algorithm so that when iterating vo-pairs in functions traverse and traverseSmSignals we create combinations of the results from o rather than taking the total sum of the results.

--- customUMLRTwcetPlugin/src/customUMLRTwcetPlugin/capsuleAnalysis.java
+++ customUMLRTwcetPlugin/src/customUMLRTwcetPlugin/capsuleAnalysis.java
@@ -1,6 +1,7 @@
 package customUMLRTwcetPlugin;
 
 import java.util.Map;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.eclipse.emf.common.util.URI;
@@ -33,7 +34,7 @@ public class capsuleAnalysis {
 	public static EList<Subcapsule> subcapsules;
 	public static TempMachine tempmachine;
 	public static enum analyzeType {RELAY, EXTERNAL, TIMEOUT, SMINIT}
-	public static analyzeType analyzetype = analyzeType.EXTERNAL;
+	public static analyzeType analyzetype = analyzeType.RELAY;
 
 	public Model loadModel(String modelPath) {
 		
@@ -72,10 +73,18 @@ public class capsuleAnalysis {
 		//AlgCapsule c = ModelAbstractor.prepCapsule(m);
 		//ModelAbstractor.printAlgCap(c);
 		
-		//AlgCapsule c = TestingRelaySignal.getTestCapsule();
-		AlgCapsule c = TestingExternalSignal.getTestCapsule();
-		//AlgCapsule c = TestingSmInit.getTestCapsule();
-		//AlgCapsule c = TestingTimeout.getTestCapsule();
+		AlgCapsule c;
+		
+		if(analyzetype == analyzeType.RELAY) {
+			c = TestingRelaySignal.getTestCapsule();
+		}else if (analyzetype == analyzeType.EXTERNAL) {
+			c = TestingExternalSignal.getTestCapsule();
+		}else if(analyzetype == analyzeType.TIMEOUT) {
+			c = TestingTimeout.getTestCapsule();
+		}else {
+			c = TestingSmInit.getTestCapsule();
+		}
+
 		
 		analyze(c);
 	}
@@ -91,15 +100,15 @@ public class capsuleAnalysis {
 		ArrayList<AlgWData> TW = new ArrayList<AlgWData>();
 		ArrayList<AlgWData> IW = new ArrayList<AlgWData>();
 		
-		
-		
-		
 		if(analyzetype == analyzeType.RELAY) {
 			for (AlgSignal signal : relaySignals) {
 				History HC = new History();
+				signal.Print();
 				AlgWData W = traverse(c, signal, HC);
 				
 				W.lookupSignal = signal;
+				
+				System.out.println("\n\n\n\n ----- Relay signal results -----\n");
 				W.Print();
 				RW.add(W);
 			}
@@ -115,15 +124,16 @@ public class capsuleAnalysis {
 				History HC = new History();
 				AlgWData E = traverseSmSignals(c, SMW, HC);
 				E.lookupSignal = signal;
+				System.out.println("\n\n\n\n ----- External signal results -----\n");
 				E.Print();
 			}
 		}
-		
-		
+			
 		
 		if(analyzetype == analyzeType.TIMEOUT) {
 			TW = timeoutAnalysis(c);
 			
+			System.out.println("\n\n\n\n ----- Timeout WCET results -----\n");
 			for(AlgWData wdata : TW) {
 				wdata.Print();
 			}
@@ -132,13 +142,22 @@ public class capsuleAnalysis {
 		if(analyzetype == analyzeType.SMINIT) {
 			IW = smInitAnalysis(c);
 			
+			System.out.println("\n\n\n\n ----- State machine Initialization WCET results -----\n");
 			for(AlgWData wdata : IW) {
 				wdata.Print();
 			}
 		}
 		
-
-
+		
+		/*
+		
+		AlgCapsule c2 = TestingSM.getTestCapsule();
+		c2.Print();
+		AlgWData smr = smTraverse(c2.statemachine, TestingSM.t1, new History());
+		System.out.println("---smr---");
+		smr.Print();
+		
+		*/
 		
 		result.RW = RW;
 		result.EW = EW;
@@ -150,7 +169,6 @@ public class capsuleAnalysis {
 	public static ArrayList<AlgWData> smInitAnalysis(AlgCapsule c) {
 		ArrayList<AlgWData> IWList = new ArrayList<AlgWData>();
 		
-		System.out.println("smInitAnalysis");
 		AlgWData SMW = new AlgWData();
 		
 		if(c.statemachine != null) {
@@ -163,16 +181,14 @@ public class capsuleAnalysis {
 			
 		}
 		
-		
 		AlgWData IW = traverseSmSignals(c, SMW, new History());
 		IW.altLookupName = c.name+".smInitial";
 		IWList.add(IW);
 		
-		
 		for(AlgSubcapsule sc : c.subcapsules) {
 			AlgWData SCIWin = wcet(sc);
 			AlgWData SCIWout = new AlgWData();
-			SCIWout.altLookupName = sc.name+".timing.timeout";
+			SCIWout.altLookupName = sc.name+".smInitial";
 			
 			for(AlgVoPair SCTWvo : SCIWin.voPairs) {
 				AlgWData mixers = new AlgWData();
@@ -220,17 +236,16 @@ public class capsuleAnalysis {
 	public static ArrayList<AlgWData> timeoutAnalysis(AlgCapsule c) {
 		ArrayList<AlgWData> TWList = new ArrayList<AlgWData>();
 		
-		System.out.println("TimeoutAnalysis");
 		AlgWData SMW = new AlgWData();
 		AlgWData TW = new AlgWData();
 		
 		ArrayList<AlgPort> timingPorts = c.getTimingPorts();
-		System.out.print(timingPorts.size());
 		for(AlgPort port : timingPorts) {
 			History HC = new History();
 			SMW = smAnalysis(c, port.signals.get(0));
 			
-			TW = traverseSmSignals(c, SMW, HC);
+			TW = traverseSmSignals(c, SMW, HC);		
+			
 			TW.lookupSignal = port.signals.get(0);
 			TWList.add(TW);
 		}
@@ -238,6 +253,7 @@ public class capsuleAnalysis {
 			
 		for(AlgSubcapsule sc : c.subcapsules) {
 			AlgWData SCTWin = wcetTW(sc, "timing.timeout");
+			
 			AlgWData SCTWout = new AlgWData();
 			SCTWout.altLookupName = sc.name+".timing.timeout";
 			
@@ -284,90 +300,84 @@ public class capsuleAnalysis {
 		return TWList;
 	}
 	
-	public static AlgWData traverse(AlgCapsule c, AlgSignal s, History HC) {
+	public static AlgWData traverse(AlgCapsule c, AlgSignal ps, History HC) {
 		AlgWData W = new AlgWData();
 		
-		if(HC.ContainsSignal(s)) {
+		if(HC.ContainsSignal(ps)) {
 			AlgVoPair vop = new AlgVoPair();
 			vop.executionTime = 9999999;
 			W.AddVoPair(vop);
 			return W;
 		}
 		
-		HC.AddSignal(s);
+		HC.AddSignal(ps);
 		
-		if(s.target.belongToSubcapsule) {
-			AlgWData Wprime = wcet(c, s);
-			for(AlgVoPair vop : Wprime.voPairs) {
-				AlgWData Wprimeprime = new AlgWData();
-				AlgVoPair relayPair = null;
-				AlgVoPair externalPair = new AlgVoPair();
-				externalPair.executionTime = vop.executionTime;
+		if(ps.target.belongToSubcapsule) {
+			AlgWData Wprime = wcet(c, ps);
+			
+			for(AlgVoPair Wpvo : Wprime.voPairs) {
+				AlgWData mixers = new AlgWData();
+				AlgVoPair newPair = new AlgVoPair();
+				newPair.executionTime = Wpvo.executionTime;
+				mixers.AddVoPair(newPair);
 				
-				for(AlgSignal sprimeprime : vop.signals) {
-					if(sprimeprime.owner.portKind == PortKind.EXTERNAL) {
-						return Wprime;
-					}
+				for(AlgSignal psp : Wpvo.signals) {
+					History HCp = new History();
+					AlgWData mixer = traverse(c, psp, HCp);
 					
-					AlgWData Wppp = traverse(c, sprimeprime, HC);
+					AlgWData newMixers = new AlgWData();
 					
-					//If we found an exit
-					if(sprimeprime.target.portKind == PortKind.RELAY) {
-						if(relayPair == null) {
-							relayPair = new AlgVoPair();
-						}
-						
-						for (AlgVoPair aavo : Wppp.voPairs) {
-							for (AlgSignal aavosig : aavo.signals) {
-								relayPair.AddSignal(aavosig);
+					for(AlgVoPair A : mixers.voPairs) {
+						for(AlgVoPair B : mixer.voPairs) {
+							AlgVoPair newMixer = new AlgVoPair();
+							
+							newMixer.executionTime = A.executionTime + B.executionTime;
+							
+							for(AlgSignal sA : A.signals) {
+								newMixer.AddSignal(sA);
 							}
+							for(AlgSignal sB : B.signals) {
+								newMixer.AddSignal(sB);
+							}
+							
+							newMixers.AddVoPair(newMixer);
 						}
 					}
 					
-					//Results from further traversing
-					else {
-						for(AlgVoPair Wpppvo : Wppp.voPairs) {
-							Wpppvo.executionTime += vop.executionTime;	
-							W.AddVoPair(Wpppvo);
-						}
-						
-					}
+					mixers = newMixers;
 				}
-
 				
-				if(relayPair != null) {
-					relayPair.executionTime = vop.executionTime;
-					W.AddVoPair(relayPair);
+				for(AlgVoPair mixer : mixers.voPairs) {
+					W.AddVoPair(mixer);
 				}
-			}
 
+			}
 			return W;
-		}
+	}
 		
-		if(s.target.portKind == PortKind.RELAY) {
+		if(ps.target.portKind == PortKind.RELAY) {
 			//Found an end
 			AlgSignal outSignal = null;
 			
-			for(AlgSignal signal : s.target.signals) {
-				if(signal.name == s.name)
+			//Get corresponding signal in capsule interface relay port
+			for(AlgSignal signal : ps.target.signals) {
+				if(signal.name == ps.name)
 					outSignal = signal;
 			}
-			
-			if(outSignal != null) {				
-				AlgVoPair outPair = new AlgVoPair();
-				outPair.executionTime = 0;
-				outPair.AddSignal(outSignal);
-				W.AddVoPair(outPair);
-			}
+						
+			AlgVoPair outPair = new AlgVoPair();
+			outPair.executionTime = 0;
+			outPair.AddSignal(outSignal);
+			W.AddVoPair(outPair);
 			
 			return W;
 		}
 		
-		if(s.target.portKind == PortKind.INTERNAL) {
-			AlgSignal signalToBeAnalyzed = null;
+		if(ps.target.portKind == PortKind.INTERNAL) {
+			AlgSignal signalToBeAnalyzed= null;
 			
-			for(AlgSignal signal : s.target.signals) {
-				if(s.name == s.name) {
+			for(AlgSignal signal : ps.target.signals) {
+				if(ps.name == signal.name) {
 					signalToBeAnalyzed = signal;
 				}
 			}
@@ -385,95 +395,65 @@ public class capsuleAnalysis {
 	public static AlgWData traverseSmSignals(AlgCapsule c, AlgWData W_voPairs, History HC) {
 		AlgWData W = new AlgWData();
 		
-		for(AlgVoPair vop : W_voPairs.voPairs) { 
-			AlgWData Wprime = new AlgWData();
-			AlgVoPair externals = new AlgVoPair();
-			externals.executionTime = vop.executionTime;
-					
-			if(vop.signals.size() <= 0) {
-				AlgVoPair noSignalPair = new AlgVoPair();
-				noSignalPair.executionTime = vop.executionTime;
-				Wprime.AddVoPair(vop);
-			}else {
-
-				if(analyzetype == analyzeType.RELAY) {
-				AlgVoPair newPair = new AlgVoPair();
-				newPair.executionTime = vop.executionTime;
-				for(AlgSignal signal : vop.signals) { 
-					
-					//State machine is sending from an external, meaning its leaving the capsule
-					if(signal.owner.portKind == PortKind.EXTERNAL) {
-						externals.AddSignal(signal);
-					}else { 
-						
-						AlgWData Wprimeprime = traverse(c, signal, HC.getCopy());
-						Wprimeprime.AddExecutionTime(vop.executionTime);
-						
-						for(AlgVoPair Wprimeprimevo : Wprimeprime.voPairs){
-							Wprime.AddVoPair(Wprimeprimevo);
-						}
-						
-					}
-				}
+		for(AlgVoPair voPair : W_voPairs.voPairs) {
+			AlgWData mixers = new AlgWData();
+			AlgVoPair newPair = new AlgVoPair();
+			newPair.executionTime = voPair.executionTime;
+			mixers.AddVoPair(newPair);
+			
+			for(AlgSignal ps : voPair.signals) {
+				
+				AlgWData mixer = new AlgWData();
+				if(voPair.signals.size() == 0) {
+					AlgVoPair vonly = new AlgVoPair();
+					//vonly.executionTime = voPair.executionTime;
+					mixer.AddVoPair(voPair);
 				}else {
-					AlgWData traverseResults = new AlgWData();
-					AlgVoPair exitSignals = new AlgVoPair();
-					AlgVoPair newPair = new AlgVoPair();
-					newPair.executionTime = vop.executionTime;
-					for(AlgSignal signal : vop.signals) { 
-						if(signal.owner.portKind == PortKind.EXTERNAL) {
-							exitSignals.AddSignal(signal);
-							externals.signals.add(signal);
-						}else {
-							//signal is being sent from an internal end port and we must continue traversing
-							AlgWData Wprimeprime = traverse(c, signal, HC.getCopy());
-							traverseResults= Wprimeprime;
-							Wprimeprime.AddExecutionTime(vop.executionTime);
+					if(ps.owner.portKind == PortKind.EXTERNAL) {
+						AlgVoPair extPair = new AlgVoPair();
+						//extPair.executionTime = voPair.executionTime;
+						extPair.AddSignal(ps);
+						mixer.AddVoPair(extPair);
+					}else {
 					
-							for(AlgVoPair Wprimeprimevo : Wprimeprime.voPairs){
-								Wprime.AddVoPair(Wprimeprimevo);
-							}
-						
+					History HCp = HC.getCopy();
+					mixer = traverse(c, ps, HCp);
+					}
+				}
+					
+				AlgWData newMixers = new AlgWData();
+					
+				for(AlgVoPair A : mixers.voPairs) {
+					for(AlgVoPair B : mixer.voPairs) {
+						AlgVoPair newMixer = new AlgVoPair();
+							
+						newMixer.executionTime = A.executionTime + B.executionTime;
+							
+						for(AlgSignal sA : A.signals) {
+							newMixer.AddSignal(sA);
 						}
-					}		
+						for(AlgSignal sB : B.signals) {
+							newMixer.AddSignal(sB);
+						}
+							
+						newMixers.AddVoPair(newMixer);
+					}
 				}
+					
+				mixers = newMixers;
+			}
+				
+			for(AlgVoPair mixer : mixers.voPairs) {
+				W.AddVoPair(mixer);
 			}
 			
-		
-
-			if(analyzetype == analyzeType.RELAY){
-				if(externals.signals.size() > 0) {
-					Wprime.AddVoPair(externals);
-				}
-				
-				for(AlgVoPair Wprimevo : Wprime.voPairs) {
-					W.AddVoPair(Wprimevo);
-				}
-
-			}else {
-				for(AlgVoPair vopvop : Wprime.voPairs) {
-					AlgVoPair newVO = new AlgVoPair();
-					newVO.executionTime = vopvop.executionTime;
-					
-					for(AlgSignal s: vopvop.signals) {
-						newVO.AddSignal(s);
-					}
-					
-					for(AlgSignal s: externals.signals) {
-						newVO.AddSignal(s);
-					}
-					W.AddVoPair(newVO);
-				}
-				
-			}
-			
-
 		}
 		
 		return W;
 	}
 	
 	public static AlgWData smAnalysis(AlgCapsule c, AlgSignal signal) {
+		
 		AlgWData SMW = new AlgWData();
 		SMW.lookupSignal = signal;
 		
@@ -489,14 +469,15 @@ public class capsuleAnalysis {
 				}
 			}
 		}
-		
+
 		return SMW;
 	}
 	
 	public static AlgWData smTraverse(AlgStatemachine sm, AlgTransition t, History history) {
-		AlgWData W = new AlgWData();
+		AlgWData W = null;
 		
 		if(history.ContainsTransition(t)) {
+			W = new AlgWData();
 			W.AddVoPair(new AlgVoPair(99999999, null));
 			return W;
 		}
@@ -517,40 +498,37 @@ public class capsuleAnalysis {
 		Wprime.AddSignals(traWcet.signals);
 		Wprime.AddSignals(tarWcet.signals);
 		
-		Wprime.Print();
 		
-		boolean foundTransition = false;
 		for(AlgTransition tprime : sm.transitions) {
 			if(tprime.source.name == t.target.name) {
-				foundTransition = true;
 				if(tprime.trigger == null) {
-					System.out.println("smTraverse("+ tprime.name +")");
 					AlgWData Wprimeprime = smTraverse(sm, tprime, history);
-					Wprimeprime.lookupTransition = tprime;
-					Wprimeprime.PrintSM();
 					
 						for(AlgVoPair vop : Wprimeprime.voPairs) {
-							AlgVoPair mergedVo = new AlgVoPair(); 
-							
+							AlgVoPair mergedVo = new AlgVoPair();
+								
 							mergedVo.executionTime = vop.executionTime + Wprime.executionTime;
-							
+								
 							for(AlgSignal s : vop.signals) {
 								mergedVo.signals.add(s);
 							}
-							for(AlgSignal s : Wprime.signals) {
+							for(AlgSignal s : Wprime.signals) {	
 								mergedVo.signals.add(s);
 							}
-							
-							mergedVo.Print();
+									
+							if(W == null) {
+								W = new AlgWData();
+							}
 							W.AddVoPair(mergedVo);
+								
 						}
-
 				}
 			}
 		}
 		
-		if(foundTransition == false) {
-			W.AddVoPair(Wprime);
+		if(W == null) {
+			W = new AlgWData();
+			W.AddVoPair(Wprime);;
 		}
 		
 		return W;
@@ -560,12 +538,9 @@ public class capsuleAnalysis {
 		AlgPort targetPort = s.target;
 		
 		for(AlgSubcapsule sc : c.subcapsules) {
-			sc.Print();
 			for(AlgPort scport : sc.ports) {
 				for(AlgSignal signal : scport.signals) {
-					signal.Print();
 					if(s.name == signal.name) {
-						
 						return sc.lookUpSignal(signal);
 					}
 				}
@@ -582,6 +557,15 @@ public class capsuleAnalysis {
 	
 	public static AlgWData wcetTW(AlgSubcapsule sc, String portsignal) {
 		return sc.lookupTW(portsignal);
+	}
+	
+	public static void WaitForInput() {
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
